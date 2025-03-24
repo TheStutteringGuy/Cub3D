fe8e1a195a2e68108f76c1017cef4ac66b9db92b
aahlaqqa <aahlaqqa@c3r3p3.1337.ma>
2025-03-24 01:48:25 +0000

    ahmed

--- bonus/check_door.c
+++ bonus/check_door.c
@@ -6,39 +6,46 @@
 /*   By: aahlaqqa <aahlaqqa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 03:16:02 by aahlaqqa          #+#    #+#             */
-/*   Updated: 2025/03/23 03:29:30 by aahlaqqa         ###   ########.fr       */
+/*   Updated: 2025/03/24 01:46:43 by aahlaqqa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3D.h"
 
-void check_valid_door(t_data *data)
+void	check_for_doors(t_data *data, int i, int j)
 {
-    int i;
-    int j;
+	if (data->mini_map[i][j] == 'D')
+	{
+		data->is_left_right = 0;
+		data->is_up_down = 0;
+		if (j > 0 && data->mini_map[i][j - 1] == '1' && data->mini_map[i][j + 1]
+			&& data->mini_map[i][j + 1] == '1')
+			data->is_left_right = 1;
+		if (i > 0 && data->mini_map[i - 1][j] == '1' && data->mini_map[i + 1]
+			&& data->mini_map[i + 1][j] == '1')
+			data->is_up_down = 1;
+		if (!data->is_left_right && !data->is_up_down)
+		{
+			printf("Eroor\nDoor should be at least sournded by 2 walls !!\n");
+			exit(1);
+		}
+	}
+}
 
-    i = 0;
-    while (data->mini_map[i])
-    {
-        j = 0;
-        while (data->mini_map[i][j])
-        {
-            if (data->mini_map[i][j] == 'D')
-            {
-                data->is_left_right = 0;
-                data->is_up_down = 0;
-                if (j > 0 && data->mini_map[i][j - 1] == '1' && data->mini_map[i][j + 1] && data->mini_map[i][j + 1] == '1')
-                    data->is_left_right = 1;
-                if (i > 0 && data->mini_map[i - 1][j] == '1' && data->mini_map[i + 1]  && data->mini_map[i + 1][j] == '1')
-                    data->is_up_down = 1;
-                if (!data->is_left_right && !data->is_up_down)
-                {
-                    printf("Eroor\nDoor should be at least sournded by 2 walls !!\n");
-                    exit(1);
-                }
-            }
-            j++;
-        }
-        i++;
-    }
+void	check_valid_door(t_data *data)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (data->mini_map[i])
+	{
+		j = 0;
+		while (data->mini_map[i][j])
+		{
+			check_for_doors(data, i, j);
+			j++;
+		}
+		i++;
+	}
 }
--- cub3D.h
+++ cub3D.h
@@ -6,7 +6,7 @@
 /*   By: aahlaqqa <aahlaqqa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 00:40:18 by aahlaqqa          #+#    #+#             */
-/*   Updated: 2025/03/24 00:51:15 by aahlaqqa         ###   ########.fr       */
+/*   Updated: 2025/03/24 01:42:36 by aahlaqqa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -213,33 +213,33 @@ typedef struct s_data
 	int				next_x;
 	int				next_y;
 	bool			move_door;
-	int is_left_right;
-	int is_up_down;
-	int	tile_size;
-	int	radius;
-	int	startx;
-	int	starty;
-	int	player_mapx;
-	int	player_mapy;
-	int	start_map_x;
-	int	start_map_y;
-	int	draw_x;
-	int	draw_y;
-	int	dx;
-	int	dy;
-	int	mini_map_x;
-	int	mini_map_y;
-	int	mini_color;
-	int	dir_end_x;
-	int	dir_end_y;
-	int	dx1;
-	int	dy1;
-	int	sx;
-	int	sy;
-	int	err;
-	int	e2;
-	int	curr_x;
-	int	curr_y;
+	int				is_left_right;
+	int				is_up_down;
+	int				tile_size;
+	int				radius;
+	int				startx;
+	int				starty;
+	int				player_mapx;
+	int				player_mapy;
+	int				start_map_x;
+	int				start_map_y;
+	int				draw_x;
+	int				draw_y;
+	int				dx;
+	int				dy;
+	int				mini_map_x;
+	int				mini_map_y;
+	int				mini_color;
+	int				dir_end_x;
+	int				dir_end_y;
+	int				dx1;
+	int				dy1;
+	int				sx;
+	int				sy;
+	int				err;
+	int				e2;
+	int				curr_x;
+	int				curr_y;
 	//////////////////////////
 }					t_data;
 
@@ -373,15 +373,19 @@ void				free_door_state(t_data *data);
 int					is_locking_at_door(t_data *data);
 void				start_interaction(t_data *data);
 void				update_doors(t_data *data, float door_timer);
-void check_valid_door(t_data *data);
-void	draw_minimap(t_data *data);
-void	draw_background(t_data *data, int minimap_size);
-void	draw_map_tiles(t_data *data);
-void	draw_player(t_data *data, int player_pixel_x, int player_pixel_y);
-void	draw_direction_line(t_data *data, int player_pixel_x, int player_pixel_y, int dir_length);
-void handle_draw_direction_line(t_data *data, int player_pixel_x, int player_pixel_y, int dir_length);
-void handle_mini_map_colors(t_data *data);
-void init_map_tiles_var(t_data *data);
+void				check_valid_door(t_data *data);
+void				draw_minimap(t_data *data);
+void				draw_background(t_data *data, int minimap_size);
+void				draw_map_tiles(t_data *data);
+void				draw_player(t_data *data, int player_pixel_x,
+						int player_pixel_y);
+void				draw_direction_line(t_data *data, int player_pixel_x,
+						int player_pixel_y, int dir_length);
+void				handle_draw_direction_line(t_data *data, int player_pixel_x,
+						int player_pixel_y, int dir_length);
+void				handle_mini_map_colors(t_data *data);
+void				init_map_tiles_var(t_data *data);
+void				init_darw_map_tile(t_data *data, int i, int j);
 ////////////////////////////////////////////////
 
 void				free_(t_data *data);
